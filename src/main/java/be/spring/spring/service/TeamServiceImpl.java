4c063ce9f70c6411b4967c5b27b1227703247f53
Tom <tompels@gmail.com>
2014-08-31 09:41:58 +0200

    removed class import

--- src/main/java/be/spring/spring/service/TeamServiceImpl.java
+++ src/main/java/be/spring/spring/service/TeamServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
 
 /**
  * Created by u0090265 on 5/10/14.
