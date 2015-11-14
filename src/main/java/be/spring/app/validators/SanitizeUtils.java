bbb753004190c953623bc0e267de3ef81bede4c8
Tom <tompels@gmail.com>
2015-11-14 18:35:15 +0100

    - unescape after sanitzing

--- src/main/java/be/spring/app/validators/SanitizeUtils.java
+++ src/main/java/be/spring/app/validators/SanitizeUtils.java
@@ -1,6 +1,7 @@
 package be.spring.app.validators;
 
 import com.google.common.base.Strings;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.owasp.html.Sanitizers;
 
 /**
@@ -9,6 +10,7 @@ import org.owasp.html.Sanitizers;
 public class SanitizeUtils {
     public static String SanitizeHtml(String text) {
         if (Strings.isNullOrEmpty(text)) return text;
-        return Sanitizers.FORMATTING.and(Sanitizers.IMAGES).and(Sanitizers.LINKS).and(Sanitizers.BLOCKS).sanitize(text);
+        String r = Sanitizers.FORMATTING.and(Sanitizers.IMAGES).and(Sanitizers.LINKS).and(Sanitizers.BLOCKS).sanitize(text);
+        return StringEscapeUtils.unescapeHtml4(r);
     }
 }
