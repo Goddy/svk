999bcf3a2c9d44761e0cc1ad712edb19dccff06b
u0090265@kuleuven.be <u0090265@kuleuven.be>
2016-08-02 22:06:01 +0200

    - no preauth on matchpolls

--- src/main/java/be/spring/app/controller/PollController.java
+++ src/main/java/be/spring/app/controller/PollController.java
@@ -2,7 +2,6 @@ package be.spring.app.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +15,6 @@ public class PollController extends AbstractController {
 
     private static final Logger logger = LoggerFactory.getLogger(PollController.class);
 
-    @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/matchPolls", method = RequestMethod.GET)
     public String getMatchPoll() {
         return "/poll/matchPolls";
